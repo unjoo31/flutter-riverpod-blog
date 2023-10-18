50077267c49ea95665ba775542e8a4c46ecc288b
unjoo31 <unjoo31@gmail.com>
2023-10-18 14:40:58 +0900

    git commit "게시글 작성 완료"

--- lib/data/repository/post_repository.dart
+++ lib/data/repository/post_repository.dart
@@ -1,18 +1,15 @@
 import 'package:dio/dio.dart';
 import 'package:flutter_blog/_core/constants/http.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
+import 'package:flutter_blog/data/model/post.dart';
+import 'package:logger/logger.dart';
 
-import '../model/post.dart';
-
-// v - p - r
-// 통신과 파싱만 함(비지니스 로직 안함)
+// V -> P(전역프로바이더, 뷰모델) -> R
 class PostRepository {
   Future<ResponseDTO> fetchPostList(String jwt) async {
-    // provider에는 ref를 접근할 수 있기 때문에 jwt를 전달받아서 사용하기
-    // dio를 사용할때는 꼭 try-catch사용하기
     try {
       // 1. 통신
-      // 헤더에 토큰이 있기 떄문에 헤더를 만들어야함
       final response = await dio.get("/post",
           options: Options(headers: {"Authorization": "${jwt}"}));
 
@@ -23,13 +20,37 @@ class PostRepository {
       List<dynamic> mapList = responseDTO.data as List<dynamic>;
       List<Post> postList = mapList.map((e) => Post.fromJson(e)).toList();
 
-      // 4. 파싱된 데이터를 다시 공통 DTO로 덮어 씌우기
+      // 4. 파싱된 데이터를 다시 공통 DTO로 덮어씌우기
       responseDTO.data = postList;
 
       return responseDTO;
     } catch (e) {
-      // 200이 아니면 catch로 감
-      return ResponseDTO(-1, "중복되는 유저명입니다", null);
+      return ResponseDTO(-1, "게시글 목록 불러오기 실패", null);
+    }
+  }
+
+  Future<ResponseDTO> fetchPost(String jwt, PostSaveReqDTO dto) async {
+    try {
+      // 1. 통신
+      final response = await dio.post("/post",
+          data: dto.toJson(),
+          options: Options(headers: {"Authorization": "${jwt}"}));
+
+      Logger().d(response.data);
+
+      // 2. ResponseDTO 파싱
+      ResponseDTO responseDTO = ResponseDTO.fromJson(response.data);
+      Logger().d(responseDTO.data);
+
+      // 3. ResponseDTO의 data 파싱
+      Post post = Post.fromJson(responseDTO.data);
+
+      // 4. 파싱된 데이터를 다시 공통 DTO로 덮어씌우기
+      responseDTO.data = post;
+
+      return responseDTO;
+    } catch (e) {
+      return ResponseDTO(-1, "게시글 작성 실패", null);
     }
   }
 }
--- lib/ui/pages/post/list_page/post_list_view_model.dart
+++ lib/ui/pages/post/list_page/post_list_view_model.dart
@@ -1,9 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_blog/data/dto/post_request.dart';
 import 'package:flutter_blog/data/dto/response_dto.dart';
+import 'package:flutter_blog/data/model/post.dart';
 import 'package:flutter_blog/data/provider/session_provider.dart';
 import 'package:flutter_blog/data/repository/post_repository.dart';
+import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-import '../../../../data/model/post.dart';
 
 // 1. 창고 데이터
 class PostListModel {
@@ -13,21 +15,43 @@ class PostListModel {
 
 // 2. 창고
 class PostListViewModel extends StateNotifier<PostListModel?> {
-  PostListViewModel(super._state, this.ref); // _state는 null을 관리한다
+  PostListViewModel(super._state, this.ref);
 
+  final mContext = navigatorKey.currentContext;
   Ref ref;
 
-  // viewmodel의 경우 꼭 notify를 붙여주기!
   Future<void> notifyInit() async {
     // jwt 가져오기
-    SessoinUser sessoinUser = ref.read(sessionProvider);
+    SessoinUser sessionStore = ref.read(sessionProvider);
+
     ResponseDTO responseDTO =
-        await PostRepository().fetchPostList(sessoinUser.jwt!);
+        await PostRepository().fetchPostList(sessionStore.jwt!);
     state = PostListModel(responseDTO.data);
+  }
+
+  Future<void> notifyAdd(PostSaveReqDTO dto) async {
+    SessoinUser sessionStore = ref.read(sessionProvider);
+
+    ResponseDTO responseDTO =
+        await PostRepository().fetchPost(sessionStore.jwt!, dto);
+
+    if (responseDTO.code == 1) {
+      Post newPost = responseDTO.data as Post; // 1. dynamic(Post) -> 다운캐스팅
+      List<Post> newPosts = [
+        newPost,
+        ...state!.posts
+      ]; // 2. 기존 상태에 데이터 추가 [전개연산자]
+      state = PostListModel(
+          newPosts); // 3. 뷰모델(창고) 데이터 갱신이 완료 -> watch 구독자는 rebuild됨.
+      Navigator.pop(mContext!); // 4. 글쓰기 화면 pop
+    } else {
+      ScaffoldMessenger.of(mContext!).showSnackBar(
+          SnackBar(content: Text("게시물 작성 실패 : ${responseDTO.msg}")));
+    }
   }
 }
 
-// 3. 창고 관리자 (view 빌드되기 직전에 생성됨)
+// 3. 창고 관리자 (View 빌드되기 직전에 생성됨)
 final postListProvider =
     StateNotifierProvider.autoDispose<PostListViewModel, PostListModel?>((ref) {
   return PostListViewModel(null, ref)..notifyInit();
