21315480d384f0a14f4f751bbc48523a9b9e036b
unjoo31 <unjoo31@gmail.com>
2023-10-18 10:26:34 +0900

    git commit "게시글 목록보기 리프래시 완료"

--- lib/ui/pages/post/list_page/post_list_page.dart
+++ lib/ui/pages/post/list_page/post_list_page.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/ui/pages/post/list_page/post_list_view_model.dart';
 import 'package:flutter_blog/ui/pages/post/list_page/wiegets/post_list_body.dart';
 import 'package:flutter_blog/ui/widgets/custom_navigator.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:logger/logger.dart';
 
-class PostListPage extends StatelessWidget {
+class PostListPage extends ConsumerWidget {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final refreshKey = GlobalKey<RefreshIndicatorState>();
 
   PostListPage({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       key: scaffoldKey,
       drawer: CustomNavigation(scaffoldKey),
@@ -18,7 +21,11 @@ class PostListPage extends StatelessWidget {
       ),
       body: RefreshIndicator(
         key: refreshKey,
-        onRefresh: () async {},
+        onRefresh: () async {
+          Logger().d("리플레시됨");
+          // 리플래시 될 때 provider를 호출해서 추가되는 게시글 리빌드 되게 함
+          ref.read(postListProvider.notifier).notifyInit();
+        },
         child: PostListBody(),
       ),
     );
